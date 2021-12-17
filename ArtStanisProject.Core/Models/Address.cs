a8112338e4ae41012bc34a0b1d8ce7e6846eec2c
Tegridy Code <marcgabel@live.dk>
2021-12-17 18:49:30 +0100

    action fix

--- ArtStanisProject.Core/Models/Address.cs
+++ ArtStanisProject.Core/Models/Address.cs
@@ -1,12 +1,12 @@
-﻿namespace ArtStanisProject.Core.Models
+﻿namespace ArtStanisProject.Core.Models{
+
+public class Address
 {
-    public class Address
-    {
-        public int Id { get; set; }
-        public string Street { get; set; }
-        public int HouseNumber { get; set; }
-        public int PostalCode { get; set; }
-        public string City { get; set; }
-        public Country Country { get; set; }
-    }
+    public int Id { get; set; }
+    public string Street { get; set; }
+    public int HouseNumber { get; set; }
+    public int PostalCode { get; set; }
+    public string City { get; set; }
+    public Country Country { get; set; }
+}
 }
